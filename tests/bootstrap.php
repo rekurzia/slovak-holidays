da5b70fb2490656aac19b79106c03183d0f741de
Vladimír Kriška <ujovlado@gmail.com>
2015-07-18 22:47:10 +0200

    Tests: set correct timezone and php interpreter

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,4 +3,6 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/Rekurzia/SlovakHolidays.php';
 
+date_default_timezone_set('Europe/Bratislava');
+
 Tester\Environment::setup();
