a1ea77818cec7251657e71f30483fce02c295e9e
Vladimír Kriška <ujovlado@gmail.com>
2015-07-19 12:19:47 +0200

    Custom function to compute Easter date. Test.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/Rekurzia/SlovakHolidays.php';
+require __DIR__ . '/../src/Rekurzia/EasterDate.php';
 
 date_default_timezone_set('Europe/Bratislava');
 
